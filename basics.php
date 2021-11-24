8e0ff71971a89b0a5f5dff0f3bf8590d9f9a2624
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2021-11-24 22:18:56 +0000

    Basico PHP

--- basics.php
+++ basics.php
@@ -73,5 +73,90 @@ class Carro{
 //$meuCarro = new Carro("preto", "opel");
 //echo $meuCarro -> msg();
 
+//Random Numbers (int)
+//echo(rand(10,100)); //Numeros [10,100]
+
+//Constantes are global
+define("NUMERO", 3); //define("NOME_DA_CONSTANTE", VALOR, CASE-INSENSITIVE(BOOL));
+function constFunc(){
+    echo NUMERO;
+    //echo numero; //apenas se define("NUMERO", 3 , True)
+}
+//constFunc();
+$intV = 3;
+$intS = "teste";
+/*
+var_dump($intV==$intS); //CMP value
+var_dump($intV===$intS); //CMP type
+var_dump($intV==2); //false
+var_dump($intV===3); //true
+*/
+
+$text=" quero ";
+$text1 = "ferias";
+
+//echo $text.$text1; //concatenação
+
+//$text .= $text1; //append
+//echo $text;
+
+//IFs
+
+$a = 3;
+if($a > 3){
+    //echo "maior ou igual que 3";
+}elseif ($a = 3){
+    //echo "igual";
+} else{
+    //echo "qualquer coisa";
+}
+
+//Case
+$favcolor = "red";
+/*
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}*/
+
+
+$x = 1;
+//While
+while($x < 10){
+    //echo $x;
+    $x++;
+}
+
+//For
+for($i = 0; $i <= 10; $i++){
+    //echo "algo ";
+}
+
+//Associative array
+$age = array("a"=>"1","b"=>"2","c"=>"3");
+
+foreach($age as $k => $k_val){
+    //echo "chave: " .$k. " valor: ". $k_val;
+}
+
+/*
+sort() - sort arrays in ascending order
+rsort() - sort arrays in descending order
+asort() - sort associative arrays in ascending order, according to the value
+ksort() - sort associative arrays in ascending order, according to the key
+arsort() - sort associative arrays in descending order, according to the value
+krsort() - sort associative arrays in descending order, according to the key
+*/
+
+
 ?>
 
