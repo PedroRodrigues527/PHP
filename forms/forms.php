31f8d9ce6b7f1bd8f53a43f0a3cc55c240f63944
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2021-11-25 09:17:46 +0000

    PHP file sends html page qith user input

---
+++ forms/forms.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<!-- When receive input, sends this .php file -->
+Nome Inserido: <?php echo$_POST["name"];?><br> <!-- <input type="text" name="name"><br> -->
+E-mail inserido: <?php echo $_POST["email"]?>
+
+</body>
+</html>
+
+
+
