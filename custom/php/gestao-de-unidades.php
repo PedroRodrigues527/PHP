cef25cef010ec0ccc509b27c31a2005b58fec012
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2021-12-23 19:34:13 +0000

    Feito a tabela da gestão de unidades

--- custom/php/gestao-de-unidades.php
+++ custom/php/gestao-de-unidades.php
@@ -17,7 +17,8 @@ else {
     }
     else{
         //Fazer pesquisa de filtragem (query)
-        $querystring = 'SELECT subitem_unit_type.id as sut_id, subitem_unit_type.name as sut_name , subitem.name as si_name, item.name as i_name FROM subitem_unit_type,subitem, item WHERE subitem_unit_type.id = subitem.unit_type_id AND subitem.item_id = item.id';
+        $querystring = 'SELECT id as sut_id, name as sut_name FROM subitem_unit_type
+                        ORDER BY id ASC';
         $queryresult = mysql_searchquery($querystring);//Query Desejado
 
         echo "<h3>Gestão de unidades - introdução</h3>";
@@ -42,12 +43,15 @@ else {
                 echo "<tr>";
                 echo "<td>" . $rowTabela['sut_id'] . "</td>"; //ID
                 echo "<td>" . $rowTabela['sut_name'] . "</td>"; //Unidade
-                $queryItemSubitem = 'SELECT item.id, item.name, subitem.id, subitem.name, subitem_unit_type.id, subitem_unit_type.name FROM item, subitem, subitem_unit_type WHERE item.id = subitem.item_id AND subitem.unit_type_id = subitem_unit_type.id';
+
+                $queryItemSubitem = 'SELECT subitem.name as si_name, item.name as i_name FROM subitem, item, subitem_unit_type 
+                                     WHERE subitem_unit_type.id = subitem.unit_type_id AND subitem.item_id = item.id AND subitem_unit_type.id = ' . $rowTabela['sut_id'];
                 $resultItemSubitem = mysql_searchquery($queryItemSubitem);//Query Desejado
                 $resultValueString = "";
+                echo "<td>";
                 //Procura dados enquanto houver resultado
                 while($rowItemSubitem = mysqli_fetch_array($resultItemSubitem, MYSQLI_NUM)){
-                    $resultValueString .= "<td>" . $rowItemSubitem[3] . " (" . $rowItemSubitem[1] . "), "; //Subitem
+                    $resultValueString .= $rowItemSubitem[0] . " (" . $rowItemSubitem[1] . "), "; //Subitem
                 }
                 echo substr_replace($resultValueString ,"", -2);
                 echo "</td>";
