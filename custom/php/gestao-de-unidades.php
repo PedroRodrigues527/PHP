39e727feacc22fd15c72a811b42a5a8af602a966
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2021-12-23 17:48:16 +0000

    Ultima coluna no display do wordpress

--- custom/php/gestao-de-unidades.php
+++ custom/php/gestao-de-unidades.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("custom/php/common.php");
 
 //Verifica se user está login e tem certa capability
@@ -36,15 +36,19 @@ else {
                      <td><b>Id</b></td>
                      <td><b>Unidade</b></td>
                      <td><b>Subitem</b></td>
-                     <td><b>Item</b></td>
                   </tr>';
 
             while($rowTabela = mysqli_fetch_assoc($queryresult)){
                 echo "<tr>";
-                echo "<td>" . $rowTabela['sut_id'] . "</td>";
-                echo "<td>" . $rowTabela['sut_name'] . "</td>";
-                echo "<td>" . $rowTabela['si_name'] . " </td>";
-                echo "<td>" . $rowTabela['i_name'] . " </td>";
+                echo "<td>" . $rowTabela['sut_id'] . "</td>"; //ID
+                echo "<td>" . $rowTabela['sut_name'] . "</td>"; //Unidade
+                $queryItemSubitem = 'SELECT item.id, item.name, subitem.id, subitem.name, subitem_unit_type.id, subitem_unit_type.name FROM item, subitem, subitem_unit_type WHERE item.id = subitem.item_id AND subitem.unit_type_id = subitem_unit_type.id';
+                $resultItemSubitem = mysql_searchquery($queryItemSubitem);//Query Desejado
+                //Procura dados enquanto houver resultado
+                while($rowItemSubitem = mysqli_fetch_array($resultItemSubitem, MYSQLI_NUM)){
+                    echo "<td>" . $rowItemSubitem[3] . " (" . $rowItemSubitem[1] . "), "; //Subitem
+                }
+                echo "</td>";
                 echo "</tr>";
             }
             echo "</tbody></table>";
