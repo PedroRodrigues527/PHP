f0c3461ac0722ff1994745d4dee8c63326de206d
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2021-12-26 11:38:32 +0000

    Tabela SemiImplementada

---
+++ custom/php/gestao-de-itens.php
@@ -0,0 +1,68 @@
+<?php
+require_once("custom/php/common.php");
+//require_once("custom/js/script.js");
+
+//FALTA:
+    //Adicionar Capability Manage items
+
+//Verifica se user está login e tem certa capability
+if(!verify_user('manage_items'))
+{
+    echo "<p>Não tem autorização para aceder a esta página</p>";
+}
+else {
+    //Verifica se existe algum elemento/valor no POST
+    if (!empty($_POST))
+    {
+        //Inserir
+        if($_POST == "inserir"){
+            echo "<h3>Gestão de itens - inserção</h3>";
+        }
+    }
+    else {
+        //Tabela
+        //Verifica se não existem tuplos na tabela subitem_unit_type
+        $itemQuery = mysql_searchquery('SELECT id, name FROM item'); //Tabela item
+        $row = mysqli_fetch_array($itemQuery, MYSQLI_NUM);
+
+        if(!$row) { //Verifica se linha esta vazia
+            echo "<p>Não há itens</p>";
+        }
+        else{
+            $tableQuery = mysql_searchquery('SELECT item.id, item.name, item.state, item_type.id, item_type.name FROM item, item_type WHERE item.id = item.item_type_id'); //Query Tabela
+
+            //Construção da tabela
+            //Tem tuplos
+            echo '<table class="mytable" style="text-align: left; width: 100%;" border="1" cellpadding="2" cellspacing="2">
+               <tbody>
+                  <tr>
+                     <td><b>tipo de item</b></td>
+                     <td><b>id</b></td>
+                     <td><b>nome do item<b></td>
+                     <td><b>estado<b></td>
+                     <td><b>ação<b></td>
+                  </tr>';
+
+            while($rowTabela = mysqli_fetch_assoc($tableQuery)) {
+                echo "<tr>";
+                echo "<td>" . $rowTabela['item_type.name'] . "</td>";
+                $previousItemType = $rowTabela['item_type.name'];
+                //FALTA JUNTAR LINHA NO ITEM_TYPE SE TIVER O MESMO NOME;
+                //Verificar se tem o mesmo item type
+                echo "<td>" . $rowTabela['item.id'] . "</td>";
+                echo "<td>" . $rowTabela['item.name'] . "</td>";
+                echo "<td>" . $rowTabela['item.state'] . "</td>";
+                if($rowTabela['item.state'] == "ativado"){
+                    echo "<td> [editar] [desativar] </td>";
+                }
+                else{
+                    echo "<td> [editar] [ativar] </td>";
+                }
+            }
+        }
+
+        echo "<h3>Gestão de itens - introdução</h3>";
+        //Form
+    }
+}
+?>
