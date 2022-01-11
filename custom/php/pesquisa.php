39606c5f1a33b0612d5820483783c2ae430912ea
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2022-01-11 22:28:14 +0000

    finalização da inserção de filters e tabela de resultados do query e inicialização incompleta da exportação dos valores e resultados num excel

--- custom/php/pesquisa.php
+++ custom/php/pesquisa.php
@@ -1,6 +1,10 @@
 <?php
 require_once("custom/php/common.php");
 
+require_once('vendor/autoload.php');
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 //Verifica se user está login e tem certa capability
 if(!verify_user('search'))
 {
@@ -89,7 +93,7 @@ else {
             echo "<li>".$filtroatr;
             echo "\n";
             echo "Operador: ";
-            echo '<select name="operador">';
+            echo '<select name="operadoratr_'.$filtroatr.'">';
             if($filtroatr != "id" && $filtroatr != "birth_date" && $filtroatr != "tutor_phone")
             {
                 //text attributes
@@ -122,12 +126,181 @@ else {
             }
         }
         echo "</ul>";
+        echo "<p>e do item: ".$_SESSION["item_name"]." uma listagem dos valores dos subitens:</p>";
+        echo "<ul>";
+        foreach($_SESSION['filtrosub_name'] as $filtrosub) {
+            echo "<li>".$filtrosub;
+            echo "\n";
+            echo "Operador: ";
+            echo '<select name="operadorsub_'.$filtrosub.'">';
+            $queryvaluetype = mysql_searchquery('SELECT value_type FROM subitem WHERE name = "'.$filtrosub.'"');
+            $valuetypesubitem = mysqli_fetch_array($queryvaluetype);
+            if($valuetypesubitem[0] == "text")
+            {
+                //text attributes
+                echo '<option value="&#61;">&#61;</option>';
+                echo '<option value="!="> != </option>';
+                echo '<option value="LIKE"> LIKE </option>';
+            }
+            else if($valuetypesubitem[0] == "enum")
+            {
+                //text attributes
+                echo '<option value="&#61;">&#61;</option>';
+                echo '<option value="!="> != </option>';
+                //echo '<option value="LIKE"> LIKE </option>';
+            }
+            else
+            {
+                //number attributes
+                echo '<option value="&#62;">&#62;</option>';
+                echo '<option value="&#62;&#61;">&#62;&#61;</option>';
+                echo '<option value="&#61;">'."&#61;".'</option>';
+                echo '<option value="<">'."<".'</option>';
+                echo '<option value="<=">'."<=".'</option>';
+                echo '<option value="!=">'."!=".'</option>';
+                //echo '<option value="LIKE">LIKE</option>';
+            }
+            echo '</select>';
+            echo "\n";
+            echo $filtrosub.": ";
+            echo '<input type="text" name="'.$filtrosub.'"/>';
+            echo "</li>";
+        }
+        foreach($_SESSION['obtersub_name'] as $obtersub) {
+            if(!in_array($obtersub, $_SESSION['filtrosub_name']))
+            {
+                echo "<li>".$obtersub;
+                echo "</li>";
+            }
+        }
+        echo "</ul>";
         echo '<input type="hidden" value="execucao" name="estado"/>
         <input type="submit" value="Submeter" />
         </form>';
     }
     else if ($_REQUEST["estado"] == "execucao") {
+        echo "<h3>Pesquisa - resultado</h3>";
 
+        if(empty($_SESSION['obteratr_name']))
+        {
+            echo "<p>Não é possível fazer a procura de crianças sem nenhum atributo da criança a obter</p>";
+            go_back_button();
+        }
+        else {
+
+            $dinamicQueryString = 'SELECT ';
+            foreach ($_SESSION['obteratr_name'] as $obteratr) {
+                $dinamicQueryString .= 'child.' . $obteratr . ", ";
+            }
+            $dinamicQueryString .= 'subitem.name as sub_name, value.value, count(*) FROM subitem, child, item, value 
+                                WHERE item.id = subitem.item_id AND value.subitem_id = subitem.id AND value.child_id = child.id AND item.id = "' . $_SESSION['item_id'] . '" AND item.name = "' . $_SESSION['item_name'] . '" ';
+
+            foreach ($_SESSION['filtroatr_name'] as $filtroatr) {
+                $dinamicQueryString .= 'AND child.' . $filtroatr . ' ';
+                if ($_REQUEST["operadoratr_" . $filtroatr] == "LIKE") {
+                    $dinamicQueryString .= 'LIKE "%' . $_REQUEST[$filtroatr] . '%" ';
+                } else {
+                    $dinamicQueryString .= $_REQUEST["operadoratr_" . $filtroatr] . ' "' . $_REQUEST[$filtroatr] . '" ';
+                }
+            }
+            if (!empty($_SESSION['filtrosub_name'])) {
+                $dinamicQueryString .= 'AND ( ';
+                foreach ($_SESSION['filtrosub_name'] as $filtrosub) {
+                    $dinamicQueryString .= '(subitem.name = "' . $filtrosub . '" AND value.value ';
+                    if ($_REQUEST["operadorsub_" . $filtrosub] == "LIKE") {
+                        $dinamicQueryString .= 'LIKE "%' . $_REQUEST[$filtrosub] . '%" ) ';
+                    } else {
+                        $dinamicQueryString .= $_REQUEST["operadorsub_" . $filtrosub] . ' "' . $_REQUEST[$filtrosub] . '" ) ';
+                    }
+                    $dinamicQueryString .= 'OR ';
+                }
+                $dinamicQueryString = substr_replace($dinamicQueryString, ")", -4);
+            }
+            //$dinamicQueryString .= ' GROUP BY child.id HAVING count(*) = '.count($_SESSION['filtrosub_name']);
+            $dinamicQueryString .= ' GROUP BY child.id ';
+            if (!empty($_SESSION['filtrosub_name'])) {
+                $dinamicQueryString .= 'HAVING count(*) = ' . count($filtrosub);
+            }
+            //echo $dinamicQueryString;//TESTE
+            $queryResultFilters = mysql_searchquery($dinamicQueryString);
+            if (mysqli_num_rows($queryResultFilters) > 0) {
+                //adicionar frase
+                $fraseQuery = '<p>Foi executado com sucesso a pesquisa do(s) registo(s) selecionando a(s) coluna(s) ';
+                foreach ($_SESSION['obteratr_name'] as $obteratr) {
+                    $fraseQuery .= $obteratr.', ';
+                }
+                $fraseQuery = substr_replace($fraseQuery, "", -2);
+                $fraseQuery .= ' da tabela child e uma coluna que contém a listagem de subitem(ns) ';
+                foreach ($_SESSION['obtersub_name'] as $obtersub) {
+                    $fraseQuery .= $obtersub.', ';
+                }
+                $fraseQuery = substr_replace($fraseQuery, "", -2);
+                $fraseQuery .= ' pertencente(s) ao item '.$_SESSION['item_name'].' usando o(s) filtro(s): ';
+                foreach ($_SESSION['filtroatr_name'] as $filtroatr) {
+                    $fraseQuery .= 'atributo '.$filtroatr.' que tem como operador de condição '.$_REQUEST['operadoratr_'.$filtroatr].' e o valor a verificar ('.$_REQUEST[$filtroatr].'); ';
+                }
+                foreach ($_SESSION['filtrosub_name'] as $filtrosub) {
+                    $fraseQuery .= 'subitem '.$filtrosub.' que tem como operador de condição '.$_REQUEST['operadorsub_'.$filtrosub].' e o valor a verificar ('.$_REQUEST[$filtrosub].'); ';
+                }
+                $fraseQuery = substr_replace($fraseQuery, "", -2);
+                $fraseQuery .= '</p>';
+                echo $fraseQuery;
+
+                //adicionar tabela
+                echo '<table class="mytable" style="text-align: left; width: 100%;" border="1" cellpadding="2" cellspacing="2">
+                <tbody>
+                  <tr>';
+                foreach ($_SESSION['obteratr_name'] as $obteratr) {
+                    echo '<th>' . $obteratr . '</th>';
+                }
+                echo '<th>Subitens e seus valores</th>';
+                echo '</tr>';
+
+                while ($row = mysqli_fetch_assoc($queryResultFilters)) {
+                    $queryStringSubitemsValues = 'SELECT ';
+                    $queryStringSubitemsValues .= 'subitem.name, value.value FROM subitem, child, item, value 
+                                WHERE item.id = subitem.item_id AND value.subitem_id = subitem.id AND value.child_id = child.id AND item.id = "' . $_SESSION['item_id'] . '" AND item.name = "' . $_SESSION['item_name'] . '" ';
+                    foreach ($_SESSION['obteratr_name'] as $obteratr) {
+                        $queryStringSubitemsValues .= 'AND child.' . $obteratr . '= "' . $row[$obteratr] . '" ';
+                    }
+
+                    if (!empty($_SESSION['obtersub_name'])) {
+                        $queryStringSubitemsValues .= 'AND ( ';
+                        foreach ($_SESSION['obtersub_name'] as $obtersub) {
+                            $queryStringSubitemsValues .= 'subitem.name = "' . $obtersub . '" ';
+                            $queryStringSubitemsValues .= 'OR ';
+                        }
+                        $queryStringSubitemsValues = substr_replace($queryStringSubitemsValues, ")", -3);
+                    }
+                    //echo $queryStringSubitemsValues;//TESTE
+                    $queryResultSubitemsValues = mysql_searchquery($queryStringSubitemsValues);
+
+
+                    if (mysqli_num_rows($queryResultSubitemsValues) > 0) {
+                        echo '<tr>';
+                        foreach ($_SESSION['obteratr_name'] as $obteratr) {
+                            echo '<td>' . $row[$obteratr] . '</td>';
+                        }
+                        echo '<td>';
+                        while ($row2 = mysqli_fetch_assoc($queryResultSubitemsValues)) {
+                            echo $row2['name'] . ': ' . $row2['value'] . '<br>';
+                        }
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+                }
+                echo "</tbody></table>";
+
+                //EXCEL - FALTA
+
+                $spreadsheet = new Spreadsheet();
+                $sheet = $spreadsheet->getActiveSheet();
+                $sheet->setCellValue('A1', 'Hello World !');
+
+                $writer = new Xlsx($spreadsheet);
+                $writer->save('hello world.xlsx');
+            }
+        }
     }
     else
     {
