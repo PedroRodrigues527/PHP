fbf2a714e7993bb90e5d9e07b999361ebb7c1efb
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2022-01-14 22:05:06 +0000

    PHP validation

---
+++ Engenharia_de_Requisitos/register.php
@@ -0,0 +1,114 @@
+<?php
+if(empty($_REQUEST)){
+    echo'<!DOCTYPE html>
+    <html>
+    
+    <head>
+        <title> GO bikes </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="img/logo.png" />
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    
+    <body>
+    
+    <section class= "header">
+        </div>
+        <nav>
+            <a href="index.html"><img src="img/logo.png" style="width:140px; height:100px;"></a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-window-close"></i>
+                <ul>
+                    <!-- <li> <a href="#">HOME</a></li> -->
+                    <li> <a href="index.html">MAIN PAGE</a></li>
+                    <li> <a href="">AJUDA</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-ellipsis-v" onclick="showMenu()"></i>
+        </nav>
+    
+    </section>
+    
+    <!-- Course -->
+    <section class="course" id="course">
+        <div class="input-box">
+    
+            <form action="" method="post">
+    
+                <div class="inputBox">
+                    <label><b>Username: </b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required>
+                    <br> <br>
+    
+                    <label><b>Password: </b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required>
+                    <br><br>
+    
+                    <label><b>Email:     </b></label>
+                    <input type="email" placeholder="Enter Email" name="e-mail" required>
+                    <br><br>
+    
+                    <div class="btn-login">
+                        <button type="submit">Register</button>
+                    </div>
+                </div>
+    
+    
+            </form>
+    
+        </div>
+    </section>
+    <!--Footer -->
+    <section class="footer">
+        <br><br><br><br>
+        <!-- <h4>Footer</h4> -->
+        <p>Social Media Contacts</p>
+        <div class="icons">
+            <!-- <i class="fa fa-star"></i> -->
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+            <!--<i class="fa fa-linkedin"></i>-->
+        </div>
+        <p>Made by Grupo 1 de ER</p>
+    </section>
+    
+    </body>
+    </html>';
+}else{
+    if(preg_match('/^[a-zA-Z0-9_.-]*$/',$_POST['username']) && preg_match('/^[a-zA-Z0-9_.-]*$/',$_POST['password']) && filter_var($_POST['e-mail'], FILTER_VALIDATE_EMAIL)) //Inserção válida no formulário
+    {
+        echo'<p>'.$_POST['username'] . ' - ' . $_POST['password'] . ' - ' . $_POST['e-mail']. '</p>';
+        //Ligação a base de dados;
+        /*$queryString = 'INSERT INTO user (username, password, email) VALUES ("'.$_POST['username'].'", "'.$_POST['password'].'", "'.$_POST['e-mail'].'");';
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "er_db";
+        $conn = mysqli_connect($servername, $username, $password, $db);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $queryResult = mysqli_query($conn, $queryString);
+        mysqli_close($conn);
+        if($queryResult)
+        {
+            echo '<script>alert("Foi registado com sucesso!");</script>';
+            header("Location: http://localhost/Engenharia_de_Requisitos/login.php");
+        }
+        else
+        {
+            echo '<script>alert("Erro ao registar a conta!");</script>';
+        }*/
+    }else{
+
+        echo '<script>if(confirm("Erro ao registar a conta!")){'.
+    header("Location: http://localhost/Engenharia_de_Requisitos/register.php").'};</script>';
+
+    }
+}
+?>
