57856df1964ae86097e79bc72373745617021cff
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2022-01-14 22:06:28 +0000

    cont do projeto ER

---
+++ Engenharia_de_Requisitos/menuprincipal.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (empty($_SESSION)) {
+    echo 'nope';
+}
+else
+{
+    if(preg_match('/^[a-zA-Z0-9_.-]*$/',$_SESSION['username']) && preg_match('/^[a-zA-Z0-9_.-]*$/',$_SESSION['password']))
+    {
+        $queryString = 'SELECT * FROM user WHERE username = "'.$_SESSION['username'].'" AND password = "'.$_SESSION['password'].'"';
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "er_db";
+        $conn = mysqli_connect($servername, $username, $password, $db);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $queryResult = mysqli_query($conn, $queryString);
+        mysqli_close($conn);
+        if(!mysqli_num_rows($queryResult))
+        {
+            echo 'nope3';
+        }
+        else
+        {
+            echo 'yep';
+        }
+    }
+    else
+    {
+        echo 'nope2';
+    }
+}
