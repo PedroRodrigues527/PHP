de158383b76ccc02b5a19fd4c2556200a02db587
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2022-01-22 20:11:40 +0000

    javascript feito, só falta adicionar id e onsubmit da validacao clientside dos formularios

--- custom/js/script.js
+++ custom/js/script.js
@@ -1,1 +1,12 @@
-//De momento, não foi necessário adicionar qualquer coisa neste ficheiro
+function validateValues(form)
+{
+    var inputs = document.getElementById(form.name).elements;
+    for (let i = 0; i < inputs.length; i++) {
+        valorinserido = inputs[i].value;
+        nomedocampo = inputs[i].name;
+        if ((valorinserido == "" || valorinserido == null) && !(nomedocampo == "email_tutor" || nomedocampo == "subitem_unit_type_name" || nomedocampo == "estado")) {
+            alert("É necessário preencher todos os campos obrigatórios!");
+            return false;
+        }
+    }
+}
